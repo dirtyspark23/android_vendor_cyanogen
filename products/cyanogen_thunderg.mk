62d44dfb333b3fb6360ca24be56aefc968a26842
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-05-06 01:08:01 -0500

    Accidental Version Change
    Change-Id: I1e2854db8f4f5522f82f65f7867bd5283a22b3ae

--- products/cyanogen_thunderg.mk
+++ products/cyanogen_thunderg.mk
@@ -40,10 +40,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.2-Thunderg
+            ro.modversion=CyanogenMod-7.1.0-Thunderg
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.2-Thunderg-KANG
+            ro.modversion=CyanogenMod-7.1.0-Thunderg-KANG
     endif
 endif
 

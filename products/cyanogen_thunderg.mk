bcd82a2eb51954909a0e831f84ca3d2ed00cf59e
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-07-03 03:10:42 -0500

    CM7 7.1.0 Stable (Thunderg)
    Change-Id: I44e0684c832ace9a8c5bb93049fd468ee244a912

--- products/cyanogen_thunderg.mk
+++ products/cyanogen_thunderg.mk
@@ -40,10 +40,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.3-Thunderg
+            ro.modversion=CyanogenMod-7.1.0-Thunderg
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.3-Thunderg-KANG
+            ro.modversion=CyanogenMod-7.1.0-Thunderg-KANG
     endif
 endif
 

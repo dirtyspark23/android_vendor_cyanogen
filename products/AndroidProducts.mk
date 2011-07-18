4aca07ae063efd6fd8964d8b44263bd65622252a
DeAngelo Mannie <dirtyspark23@gmail.com>
2011-07-17 19:30:12 -0500

    Add ThunderC
    Change-Id: I162a601dbf147aaf031636021e2602ccbf552942

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -37,6 +37,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_smb_b9701.mk \
     $(LOCAL_DIR)/cyanogen_speedy.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
+    $(LOCAL_DIR)/cyanogen_thunderc.mk \
     $(LOCAL_DIR)/cyanogen_thunderg.mk \
     $(LOCAL_DIR)/cyanogen_u8220.mk \
     $(LOCAL_DIR)/cyanogen_vega.mk \
